6fe1e33824c39aa703edbfc44c8c3195f35f0948
uo287585 <uo287585@uniovi.es>
2024-07-21 23:51:57 +0200

    ass task

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import './App.css';
 import {useState,useRef,useEffect} from "react";
 
 function App() {
-  let [task,setTask] = useState([{name:"Task1",place:"School",priority:0},
+  let [name,setName]=useState("")
+  let [place,setPlace]=useState("")
+  let [priority,setPriority]=useState(0)
+
+  let [tasks,setTasks] = useState([{name:"Task1",place:"School",priority:0},
     {name:"Clean",place:"House",priority:0}]) 
+
+  let addTask=()=>{
+    let newTask = {name:name,place:place,priority:priority}
+    setTasks([...tasks,newTask])
+  }
   return ( 
-    <>
+    
       <div>
-        {task.map(t=><li>
-          <b>{t.name}</b>
-          <div>Priority: {t.priority}</div>
-          <div>{t.place}</div>
-          </li>)}
+        <h2>Add task</h2>
+        <input type="text" placeholder="name" onChange={(e)=>{setName(e.currentTarget.value)}}></input>
+        <input type="text" placeholder="place" onChange={(e)=>{setPlace(e.currentTarget.value)}}></input>
+        <input type="number" placeholder="0" onChange={(e)=>{setPriority(e.currentTarget.value)}}></input>
+        <button onClick={addTask}>Add task</button>
+        <ul>
+          {tasks.map(t=><li>
+            <b>{t.name}</b>
+            <div>Priority: {t.priority}</div>
+            <div>{t.place}</div>
+            </li>)}
+        </ul>
       </div>
-    </>
+    
   );
 }
 
